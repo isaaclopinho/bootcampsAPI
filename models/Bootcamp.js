ef8aa06f4a96d9c659b58e90b94e0d65f9edbb01
isaaclopinho <isaaclopinho@gmail.com>
2020-05-07 14:22:19 -0300

    added slugify middleware to bootcamp schema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -97,6 +98,12 @@ const BootcampSchema = new mongoose.Schema(
       type: Date,
       default: Date.now
     }
+});
+
+
+BootcampSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {lower: true});
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
