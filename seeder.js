dfa71b0b29fffa4eac250c4a49bc26466daea698
isaaclopinho <isaaclopinho@gmail.com>
2020-05-09 17:40:17 -0300

    Course model has been created and added to seeder

--- seeder.js
+++ seeder.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const colors = require("colors");
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -18,9 +19,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/data/bootcamps.json`, "utf-8")
 );
 
+//READ JSON FILES
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/courses.json`, "utf-8")
+);
+
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
 
     console.log("Data Imported...".green.inverse);
     process.exit();
@@ -32,6 +39,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
 
     console.log("Data Deleted...".red.inverse);
     process.exit();
